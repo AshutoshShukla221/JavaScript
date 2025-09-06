aea46913449d0f8207a263928c781f22bf346010
AshutoshShukla221 <ashutoshukla217@gmail.com>
2025-09-06 21:47:58 +0000

    conatiner add

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // as we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("AshutoshShukla221")
+
+
+let name = "Ashutosh"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint =>used to store large integer data
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not yet defdined in the program
+// symbol => unique
+ 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
