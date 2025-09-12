e11d4a7214edfc9b66806c08bed09d89e26a5fb0
AshutoshShukla221 <ashutoshukla217@gmail.com>
2025-09-12 23:31:17 +0000

    add commit

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,32 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+// console.log("2">1)
+// console.log("02">1)
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+//The reason is that an equality check== and comparisons >,<,>=,<= work differently
+//comparisons covert null to a number,treating it as a 0.
+
+//that's why(ln:12) null>=0 and (ln:10)null>0 is false.
+
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+//Strict Check(===)
+
+console.log("2"===2) //checks both value and datatype
+console.log("2"==2) //checks only value
+
+
+
+
+
